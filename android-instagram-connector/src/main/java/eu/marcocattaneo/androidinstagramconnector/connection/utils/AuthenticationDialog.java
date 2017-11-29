c8d1ecfb97baea34ca94577269d6fed98daf84d3
Vishnu Haridas <hvishnu999@gmail.com>
2017-11-29 17:18:14 +0530

    Added layout resource for Authentication Dialog.

--- android-instagram-connector/src/main/java/eu/marcocattaneo/androidinstagramconnector/connection/utils/AuthenticationDialog.java
+++ android-instagram-connector/src/main/java/eu/marcocattaneo/androidinstagramconnector/connection/utils/AuthenticationDialog.java
@@ -2,26 +2,20 @@ package eu.marcocattaneo.androidinstagramconnector.connection.utils;
 
 import android.app.Activity;
 import android.app.Dialog;
-import android.graphics.Point;
-import android.os.Build;
 import android.os.Bundle;
-import android.view.Display;
-import android.view.ViewGroup;
-import android.view.WindowManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.FrameLayout;
+
+import eu.marcocattaneo.androidinstagramconnector.R;
 
 public class AuthenticationDialog extends Dialog {
 
-    public static AuthenticationDialog newInstnace(Activity activity, String url, String urlCallback) {
-        AuthenticationDialog f = new AuthenticationDialog(activity, url, urlCallback);
-        return f;
+    public static AuthenticationDialog newInstance(Activity activity, String url, String urlCallback) {
+        return new AuthenticationDialog(activity, url, urlCallback);
     }
 
-    public AuthenticationDialog(Activity activity, String url, String urlCallback) {
+    private AuthenticationDialog(Activity activity, String url, String urlCallback) {
         super(activity);
-        this.mActivity = activity;
         this.mUrl = url;
         this.mUrlCallback = urlCallback;
     }
@@ -30,17 +24,16 @@ public class AuthenticationDialog extends Dialog {
 
     private String mUrlCallback;
 
-    private Activity mActivity;
-
     private OnHttpCallback mOnHttpCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        WebView wv = new WebView(mActivity);
+        setContentView(R.layout.dialog_authentication);
+
+        WebView wv = (WebView) findViewById(R.id.webView);
         wv.loadUrl(mUrl);
-        wv.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
 
         wv.setWebViewClient(new WebViewClient() {
             @Override
@@ -55,24 +48,6 @@ public class AuthenticationDialog extends Dialog {
                 return false;
             }
         });
-
-        int width = 0;
-        int height = 0;
-        Point size = new Point();
-        WindowManager w = mActivity.getWindowManager();
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            w.getDefaultDisplay().getSize(size);
-            width = size.x;
-            height = size.y;
-        } else {
-            Display d = w.getDefaultDisplay();
-            width = d.getWidth();
-            height = d.getHeight();
-        }
-
-
-        setContentView(wv, new FrameLayout.LayoutParams(width, (int) (height * 0.6)));
     }
 
     /**
